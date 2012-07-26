7733cd8d39489503b75cbd32bdb8f0d0b6a984dc
Todd H. Gardner <todd@wocketware.com>
2012-07-26 15:13:45 -0500

    Added sinon, added server expectations to tests

--- public/test/js/app.tests.js
+++ public/test/js/app.tests.js
@@ -1,20 +1,35 @@
 
-var $dom = $('#qunit-fixture');
-
 test( "window.onStatusSubmit() sends textarea data to server and renders result", function() {
   
 	// Arrange
-	$dom.append('<textarea>Some Value</textarea>');
-	$dom.append('<div id="statuses">');
+	var server = sinon.fakeServer.create();
+	server.respondWith("POST", "/soliloquy/api/status/",
+		[200, { "Content-Type": "application/json" },
+		'{ "text": "Hey there" }']);
 
 	// Act
 	window.onStatusSubmit();
+	server.respond();
 
 	// Assert
-	// This sucks! I have to wait for ajax to finish and I don't control what
-	// the server will return.
 	strictEqual($('textarea').val(), '', 'text area was cleared');
-	ok($('#statuses li').length > 0, 'status was added to dom');
+	strictEqual($('#statuses li').html(), 'Hey there', 'status was added to dom');
 
 });
 
+test( "window.getStatus() gets status from server and renders in list", function() {
+  
+	// Arrange
+	var server = sinon.fakeServer.create();
+	server.respondWith("GET", "/soliloquy/api/status/",
+		[200, { "Content-Type": "application/json" },
+		'[{ "text": "Hi" }, { "text": "You" }, { "text": "Guys" }]']);
+
+	// Act
+	window.getStatus();
+	server.respond();
+
+	// Assert
+	strictEqual($('#statuses li').length, 3, 'statuses were added to dom');
+
+});
