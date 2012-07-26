c940ac1ca80847668fc6c40c2aeb7597a88953ce
Todd H. Gardner <todd@wocketware.com>
2012-07-26 16:06:54 -0500

    Refactored data into models/collections

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,22 @@
+window.statusCollection = new window.collections.StatusCollection();
+
 window.onStatusSubmit = function() {
-  $.ajax({
-    url: '/soliloquy/api/status/',
-    type: 'POST',
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify({ text : $(this).find('textarea').val() }),
-    success: function(data) {
-      $('textarea').val('');
-      $('#statuses').append('<li class="status">' + data.text + '</li>');
-    }
-  });
+  window.statusCollection.create({ text: $(this).find('textarea').val() });
   return false;
 };
 
-window.getStatus = function () {
-  $.ajax({
-    url: '/soliloquy/api/status/',
-    dataType: 'json',
-    success: function(data) {
-      var $statuses = $('#statuses');
-      for(var i = 0; data.length > i; i++) {
-        $statuses.append('<li class="status">' + data[i].text + '</li>');
-      }
-    }
-  });
+window.writeAllStatus = function (collection) {
+  collection.each(window.writeStatus);
+};
+
+window.writeStatus = function (model) {
+  $('textarea').val('');
+  $('#statuses').append('<li class="status">' + model.get('text') + '</li>');
 };
 
 jQuery(function($) {
   $('form').on('submit', window.onStatusSubmit);
-  window.getStatus();
+  window.statusCollection.on('add', window.writeStatus);
+  window.statusCollection.on('reset', window.writeAllStatus);
+  window.statusCollection.fetch();
 });
