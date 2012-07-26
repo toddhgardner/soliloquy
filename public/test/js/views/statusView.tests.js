876e57d34b0b2e203d48a7ce3c3aa808eb6fbcfc
Todd H. Gardner <todd@wocketware.com>
2012-07-26 16:44:51 -0500

    Refactored tests to use require

--- public/test/js/views/statusView.tests.js
+++ public/test/js/views/statusView.tests.js
@@ -1,46 +1,54 @@
 
-sinon.stub(window.views.StatusView.prototype, 'initialize');
-
-test( "view.onStatusSubmit() creates status model in collection", function() {
-  
-	// Arrange
-	var fakeCollection = {
-		create: sinon.spy()
-	};
-
-	var view = new window.views.StatusView({
-		el: $('#qunit-fixture'),
-		collection: fakeCollection
-	});
-
-	var fakeEvent = {
-		preventDefault: sinon.spy()
-	};
-
-	// Act
-	var result = view.onStatusSubmit(fakeEvent);
-
-	// Assert
-	ok(!result, 'returned false');
-	ok(fakeCollection.create.calledOnce, 'create was called');
+require(['../js/views/statusView'], function(StatusView) {
+	sinon.stub(StatusView.prototype, 'initialize');
 });
 
-test( "view.writeStatus() writes text to dom", function() {
-  
-	// Arrange
-	var fakeModel = {
-		get: sinon.spy()
-	};
+asyncTest( "view.onStatusSubmit() creates status model in collection", function() {
+	require(['../js/views/statusView'], function(StatusView) {
+		start();
 
-	var view = new window.views.StatusView({
-		el: $('#qunit-fixture')
+		// Arrange
+		var fakeCollection = {
+			create: sinon.spy()
+		};
+		
+		var view = new StatusView({
+			el: $('#qunit-fixture'),
+			collection: fakeCollection
+		});
+
+		var fakeEvent = {
+			preventDefault: sinon.spy()
+		};
+
+		// Act
+		var result = view.onStatusSubmit(fakeEvent);
+
+		// Assert
+		ok(!result, 'returned false');
+		ok(fakeCollection.create.calledOnce, 'create was called');
 	});
+});
+
+asyncTest( "view.writeStatus() writes text to dom", function() {
+	require(['../js/views/statusView'], function(StatusView) {
+		start();
+
+		// Arrange
+		var fakeModel = {
+			get: sinon.spy()
+		};
+
+		var view = new StatusView({
+			el: $('#qunit-fixture')
+		});
 
 
-	// Act
-	view.writeStatus(fakeModel);
+		// Act
+		view.writeStatus(fakeModel);
 
-	// Assert
-	strictEqual($('#statuses li').length, 1, 'statuses were written to dom');
+		// Assert
+		strictEqual($('#statuses li').length, 1, 'statuses were written to dom');
 
+	});
 });
