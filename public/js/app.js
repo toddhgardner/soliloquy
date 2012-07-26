9e40eb10e888acd34bb560aa12c3f2f47a0d18c1
Todd H. Gardner <todd@wocketware.com>
2012-07-26 16:27:41 -0500

    Refactored to Views

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,6 @@
-window.statusCollection = new window.collections.StatusCollection();
-
-window.onStatusSubmit = function() {
-  window.statusCollection.create({ text: $(this).find('textarea').val() });
-  return false;
-};
-
-window.writeAllStatus = function (collection) {
-  collection.each(window.writeStatus);
-};
-
-window.writeStatus = function (model) {
-  $('textarea').val('');
-  $('#statuses').append('<li class="status">' + model.get('text') + '</li>');
-};
-
 jQuery(function($) {
-  $('form').on('submit', window.onStatusSubmit);
-  window.statusCollection.on('add', window.writeStatus);
-  window.statusCollection.on('reset', window.writeAllStatus);
-  window.statusCollection.fetch();
+  var statusView = new window.views.StatusView({
+    el: $('#status-container'),
+    collection: new window.collections.StatusCollection()
+  });
 });
