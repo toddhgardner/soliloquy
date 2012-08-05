2ec4926050f95ed394285d692f0dc8612403665e
Todd H. Gardner <todd@wocketware.com>
2012-08-05 15:59:08 -0500

    refactored the tests so they pass, but they are ugly.

--- public/test/js/app.tests.js
+++ public/test/js/app.tests.js
@@ -1,20 +1,23 @@
 
-var $dom = $('#qunit-fixture');
-
-test( "window.onStatusSubmit() sends textarea data to server and renders result", function() {
+asyncTest("window.onStatusSubmit() sends textarea data to server and renders result", function() {
   
 	// Arrange
+	var $dom = $('#qunit-fixture');
 	$dom.append('<textarea>Some Value</textarea>');
 	$dom.append('<div id="statuses">');
 
 	// Act
-	window.onStatusSubmit();
+	window.onStatusSubmit.call($dom);
 
-	// Assert
+	// wait for ajax to be done
 	// This sucks! I have to wait for ajax to finish and I don't control what
 	// the server will return.
-	strictEqual($('textarea').val(), '', 'text area was cleared');
-	ok($('#statuses li').length > 0, 'status was added to dom');
-
+	setTimeout(function () {
+		start();
+		
+		// Assert
+		strictEqual($dom.find('textarea').val(), '', 'text area was cleared');
+		ok($dom.find('#statuses li').length > 0, 'status was added to dom');
+	
+	}, 100);
 });
-
