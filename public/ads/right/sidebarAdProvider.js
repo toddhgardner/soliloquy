4487d30e391a7f82b41390b5fe6d603d872684aa
Todd H. Gardner <todd@wocketware.com>
2015-04-20 15:05:13 -0700

    fix hoisting

--- public/ads/right/sidebarAdProvider.js
+++ public/ads/right/sidebarAdProvider.js
@@ -10,6 +10,17 @@ jQuery(function ($) {
 
   var options = _.extend({}, defaults, window.SIDEBAR_ADS);
 
+  var ads = [
+    {
+      url: "/ads/right/ad1.html"
+    },
+    {
+      url: "/ads/right/ad2.html"
+    },
+    {
+      url: "/ads/right/ad3.html"
+    }
+  ];
   var container = $(options.container);
   var i;
 
@@ -44,17 +55,6 @@ jQuery(function ($) {
     });
   }
 
-  var ads = [
-    {
-      url: "/ads/right/ad1.html"
-    },
-    {
-      url: "/ads/right/ad2.html"
-    },
-    {
-      url: "/ads/right/ad3.html"
-    }
-  ];
   function getRandomAd() {
     return ads[Math.floor(Math.random() * ads.length)];
   }
